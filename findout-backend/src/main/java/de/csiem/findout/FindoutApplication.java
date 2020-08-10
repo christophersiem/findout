b56b24f653b9ad3a1f36aac212a6c386dc55f4d5
christophersiem <cman1609@gmail.com>
2020-08-10 20:09:53 +0200

    Project setup

---
+++ findout-backend/src/main/java/de/csiem/findout/FindoutApplication.java
@@ -0,0 +1,13 @@
+package de.csiem.findout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindoutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindoutApplication.class, args);
+    }
+
+}
---
+++ findout-backend/src/test/java/de/csiem/findout/FindoutApplicationTests.java
@@ -0,0 +1,13 @@
+package de.csiem.findout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindoutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
