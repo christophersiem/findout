801216675b6be932c6f7dd7f0d29407972d6da0a
christophersiem <65181238+christophersiem@users.noreply.github.com>
2020-08-31 23:31:50 +0200

    Add points field

--- findout-frontend/src/pages/AddVoting.js
+++ findout-frontend/src/pages/AddVoting.js
@@ -3,13 +3,33 @@ import TextField from '@material-ui/core/TextField';
 import AddIcon from '@material-ui/icons/Add';
 import {IconButton} from "@material-ui/core";
 import RemoveIcon from '@material-ui/icons/Remove';
+import Button from "@material-ui/core/Button";
+import {addNewQuestion} from "../utils/question-utils";
+
 
 export default function AddVoting() {
 
-    const [optionList,setOptionList] = useState(["option1"]);
+
+    const [question, setQuestion] = useState("");
+    const [optionList, setOptionList] = useState([
+        {option: "", points:""},
+    ]);
+
+    const handleChange = (e, index) => {
+        const {name, value} = e.target;
+        const list = [...optionList];
+        list[index][name] = value;
+
+        setOptionList(list)
+    }
+
+    const handleChangeQuestion = (event) => {
+        setQuestion(event.target.value);
+    };
 
     const handleAddClick = () => {
-        setOptionList([...optionList, "option2"]);
+        setOptionList([...optionList, {option: "", points: ""}]);
+
     };
 
     const handleRemoveClick = () => {
@@ -18,37 +38,68 @@ export default function AddVoting() {
         setOptionList(list);
     };
 
+    const questionToAdd = {
+        question:question,
+        optionList:optionList
+    }
+
+    function handleSubmit() {
+        console.log(optionList)
+        addNewQuestion(questionToAdd)
+            .catch((e) => console.error(e))
+    }
+
     return (
         <>
             <h2>Add new Voting</h2>
             <form noValidate autoComplete="off">
 
                 <TextField
+
                     id="outlined-secondary"
                     label="Enter question"
                     variant="outlined"
                     color="black"
+                    onChange={handleChangeQuestion}
                 />
 
             </form>
 
             <h2>Options</h2>
             <form noValidate autoComplete="off">
-                {optionList.map((x,i)=> {
-                return(
-                    <TextField
-                        style={{margin:10}}
-                        id="outlined-secondary"
-                        label="Enter an option"
-                        variant="outlined"
-                        color="black"
-                    />
-                )
-                })
-                }
+                {optionList.map((item, i) => {
+                    return (
+                        <div key={i}>
+                            <TextField
+                                onChange={e => handleChange(e, i)}
+                                value={item.option}
+                                style={{margin: 10}}
+                                name="option"
+                                id="outlined-secondary"
+                                label="Enter an option"
+                                variant="outlined"
+                                color="black"
+                            />
+                            <TextField
+                                type="number"
+                                onChange={e => handleChange(e, i)}
+                                value={item.points}
+                                name="points"
+                                id="standard-number"
+                                label="Points"
+                                type="number"
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                            />
 
+
+                        </div>
+                    )
+                })}
 
             </form>
+
             <IconButton
                 onClick={handleAddClick}>
                 <AddIcon style={{marginTop: 10, fontSize: "2.5rem"}}/>
@@ -57,6 +108,13 @@ export default function AddVoting() {
                 onClick={handleRemoveClick}>
                 <RemoveIcon style={{marginTop: 10, fontSize: "2.5rem"}}/>
             </IconButton>
+            <Button
+                onClick={handleSubmit}
+                variant="contained"
+                style={{margin: 20}}
+            >Create</Button>
+            <pre>{JSON.stringify(optionList)}</pre>
         </>
+
     )
 }
