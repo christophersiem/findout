a2795f7719f1a4ba8569cf62df33bc4954c2cd69
christophersiem <65181238+christophersiem@users.noreply.github.com>
2020-09-02 09:42:03 +0200

    Add Routing

--- findout-frontend/src/Navigation.js
+++ findout-frontend/src/Navigation.js
@@ -1,14 +1,27 @@
 import React from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+
+} from "react-router-dom";
 import Header from "./components/Header";
-import MainContent from "./components/MainContent";
 import LabelBottomNavigation from "./navigation/LabelBottomNavigation";
+import AddVoting from "./pages/AddVoting";
 
 export default function Navigation() {
     return (
         <>
             <Header/>
-            <MainContent/>
+            <Router>
+                <Switch>
+                    <Route path="/add">
+            <AddVoting/>
+                    </Route>
+                </Switch>
+            </Router>
             <LabelBottomNavigation/>
+
         </>
 
     )
