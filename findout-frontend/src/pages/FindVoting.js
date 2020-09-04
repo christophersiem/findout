aa812a69d6b7f7fec07e5b2cdd099490f5ea3e9f
christophersiem <65181238+christophersiem@users.noreply.github.com>
2020-09-04 12:03:25 +0200

    Display fetched Voting

--- findout-frontend/src/pages/FindVoting.js
+++ findout-frontend/src/pages/FindVoting.js
@@ -1,8 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import {fetchQuestionById} from "../utils/question-utils";
+
+const useStyles = makeStyles(() => ({
+    createButton: {
+        borderRadius: "20px",
+        backgroundColor: "black",
+        color: "white"
+    },
+}))
 
 export default function FindVoting(){
+    const classes = useStyles();
+    const [voting,setVoting] = useState([])
+    const [id, setId] =useState("")
+
+    function handleSubmit() {
+        fetchQuestionById(id)
+            .then(data =>{
+                setVoting(data)
+            })
+            .catch((e) => console.error(e))
+    }
+    console.log(voting)
+
+const handleChange =(event) =>{
+        setId(event.target.value)
+}
+
 return(
     <Grid
         container
@@ -13,6 +41,7 @@ return(
         <h2>Find Voting</h2>
 
         <TextField
+            onChange={handleChange}
             name="id"
             type="text"
             id="outlined-secondary"
@@ -21,6 +50,19 @@ return(
             variant="outlined"
             style={{margin: 10}}
         />
+        <Grid item>
+            <Button
+                className={classes.createButton}
+                onClick={handleSubmit}
+                variant="contained"
+                style={{margin: 20}}
+            >Find</Button>
+        </Grid>
+       <p> ID: {voting.id}</p>
+        <p> Question: {voting.question}</p>
+        <p>{voting.optionList && voting.optionList.map((item) =>(
+            <p>{item.option}</p>
+            ))}</p>
     </Grid>
 )
 }
