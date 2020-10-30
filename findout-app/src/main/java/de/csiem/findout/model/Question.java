0878188259e764c57075edca2f2f839f4baaee0d
christophersiem <christopher.siem@gmail.com>
2020-10-30 19:07:22 +0100

    Add question Doc

--- findout-app/src/main/java/de/csiem/findout/model/Question.java
+++ findout-app/src/main/java/de/csiem/findout/model/Question.java
@@ -3,6 +3,8 @@ package de.csiem.findout.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
@@ -11,8 +13,10 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Document("question")
 
 public class Question {
+    @Id
     private String id;
     private String question;
     private List<Object> optionList;
